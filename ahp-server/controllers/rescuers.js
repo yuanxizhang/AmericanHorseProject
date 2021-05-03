195f5c232aae60b8a08a9af992717b31bbbdc4a0
Elizabeth <Elizabeth.Rights812@gmail.com>
2021-05-03 18:36:48 -0400

    0007_done

---
+++ ahp-server/controllers/rescuers.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const router = express.Router();
+
+const Rescuer = require("../db/models/Rescuers");
+
+router.get("/", (req, res) => {
+  Rescuer.find({}).then((rescuer) => {
+    res.json(rescuer);
+  });
+});
+
+router.get("/:id", (req, res) => {
+  let parameter = req.params.id;
+  Rescuer.findById(parameter).then((rescuer) => {
+    res.json(rescuer);
+  });
+});
+
+router.post("/", (req, res) => {
+  Rescuer.create(req.body).then((rescuer) => {
+    res.json(rescuer);
+  });
+});
+
+router.put("/:id", (req, res) => {
+  let updatedRescuer = req.body;
+  Rescuer.findOneAndUpdate({ _id: req.params.id }, updatedRescuer, {
+    new: true,
+  }).then(() => {
+    Rescuer.find({}).then((rescuer) => {
+      res.json(rescuer);
+    });
+  });
+});
+
+router.delete("/:id", (req, res) => {
+  Rescuer.findByIdAndDelete({ _id: req.params.id }).then(() => {
+    Rescuer.find({}).then((rescuer) => {
+      res.json(rescuer);
+    });
+  });
+});
+
+module.exports = router;
