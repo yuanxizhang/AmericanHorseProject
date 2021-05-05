b3e4866fe0b19db532a27d3cced0e274351b6aa7
Yancy Zhang <yuanxi.zhang@gmail.com>
2021-05-04 17:40:39 -0700

    Update server.js, adoption.route.js

--- ahp-server/routes/secure/adoption.route.js
+++ ahp-server/routes/secure/adoption.route.js
@@ -5,16 +5,16 @@ const adoptionRouter = new Router();
 // Get all adoption requests
 adoptionRouter.route('/adoptionRequests').get(AdoptionRequestController.getAllRequests);
 
-// Get one adoption request by cuid
+// Get one adoption request by id
 adoptionRouter.route('/adoptionRequests/:id').get(AdoptionRequestController.getOneRequest);
 
 // Add a new adoption request
 adoptionRouter.route('/adoptionRequests').post(AdoptionRequestController.createRequest);
 
-// Update an adoption request by cuid
+// Update an adoption request by id
 adoptionRouter.route('/adoptionRequests/:id').put(AdoptionRequestController.updateRequest);
 
-// Delete an adoption request by cuid
+// Delete an adoption request by id
 adoptionRouter.route('/adoptionRequests/:id').delete(AdoptionRequestController.deleteRequest);
 
 export default adoptionRouter;
--- ahp-server/server.js
+++ ahp-server/server.js
@@ -3,12 +3,15 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
-dotenv.config()  
-const app = express();
-const PORT = 5000;
-const MONGODB_URI = "mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.cw71g.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+dotenv.config();
 
-mongoose.connect(MONGODB_URI,{ useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.DB,{ 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true, 
+    useFindAndModify: false})
     .then(() => console.log("MongoDB Database Connected Successfully"))
     .catch(err => console.log(err));
 
