4e9945e5612cf7c88e6b548f3e8c96b92ab9be61
laurencun <lauren.busbee@gmail.com>
2021-05-13 15:01:21 -0500

    fixed controller/users.js methods

--- ahp-server/controllers/users.js
+++ ahp-server/controllers/users.js
@@ -94,13 +94,13 @@ exports.passwordRedirect = async (req, res) => {
 // ***********************************************//
 exports.getCurrentUser = async (req, res) => {
   const user = await User.findById(req.user._id)
-    .populate('cyclist')
-    .populate('bikeshop');
+    .populate('adopter')
+    .populate('rescuer');
 
   res.json({
     ...user.toObject(),
-    cyclist: user.cyclist,
-    bikeshop: user.bikeshop
+    adopter: user.adopter,
+    rescuer: user.rescuer
   });
 };
 
@@ -109,7 +109,7 @@ exports.getCurrentUser = async (req, res) => {
 // ***********************************************//
 exports.updateCurrentUser = async (req, res) => {
   const updates = Object.keys(req.body);
-  const allowedUpdates = ['name', 'email', 'password', 'avatar'];
+  const allowedUpdates = ['name', 'email', 'password'];
   const isValidOperation = updates.every((update) =>
     allowedUpdates.includes(update)
   );
@@ -170,20 +170,9 @@ exports.deleteUser = async (req, res) => {
 };
 
 // ***********************************************//
-// Upload avatar
+// Upload password
 // ***********************************************//
-exports.uploadAvatar = async (req, res) => {
-  try {
-    const response = await cloudinary.uploader.upload(
-      req.files.avatar.tempFilePath
-    );
-    req.user.avatar = response.secure_url;
-    await req.user.save();
-    res.json(response);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-};
+
 
 exports.updatePassword = async (req, res) => {
   try {
