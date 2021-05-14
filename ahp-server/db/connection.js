5fe69a31a9e206c455310ffb974962ac72d1c20c
laurencun <lauren.busbee@gmail.com>
2021-05-14 15:18:54 -0500

    confirmed mongodb uri connected

--- ahp-server/db/connection.js
+++ ahp-server/db/connection.js
@@ -4,11 +4,4 @@ mongoose.Promise = Promise;
 
 const mongoURI = process.env.NODE_ENV === 'production' ? process.env.DB_URL : '';
 
-mongoose.connect(process.env.DB,{ 
-    useNewUrlParser: true, 
-    useUnifiedTopology: true, 
-    useFindAndModify: false})
-    .then(() => console.log("MongoDB Database Connected Successfully"))
-    .catch(err => console.log(err));
-
 module.exports = mongoose;
--- ahp-server/server.js
+++ ahp-server/server.js
@@ -1,14 +1,37 @@
-const app = require('./app');
+const dotenv = require('dotenv');
 const express = require('express');
+const mongoose = require('mongoose');
 const cors = require('cors');
-const PORT = 5000;
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+
+// Connect to Mongo
+mongoose.connect(process.env.MONGODB_URL,{ 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true, 
+    useFindAndModify: false})
+    .then(() => console.log("MongoDB Database Connected Successfully"))
+    .catch(err => console.log(err));
+
 
 app.use(cors());
+
+
+app.get('/', (req, res) => {
+    res.json({message: 'American Horse Project RESTful API'});
+  });
 
 /**
  * Start Express server.
  */
 
+app.use('/', (req, res) => {
+res.send('Hello world')
+});
 
 app.listen(PORT, function() {
     console.log("Express server is running on Port: " + PORT);
