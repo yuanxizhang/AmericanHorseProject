aeacda740bcee5e5ebe7cf033396caa351f34f4d
Elizabeth <Elizabeth.Rights812@gmail.com>
2021-06-15 12:01:12 -0400

    Header,Footer,Navbar Initialized

---
+++ the-american-horse-project/src/components/common/Header/Header.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import logo from './logo.svg';
+import { Header } from './components/common'
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <code>The American Horse Project</code>
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          
+        </a>
+      </header>
+      <Header />
+    </div>
+  );
+}
+export default App;
