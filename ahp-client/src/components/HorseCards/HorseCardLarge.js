d363be2f7df02dc54a63e9484f4415fade099795
tracilevine <televine@gmail.com>
2021-06-15 13:24:59 -0400

    added horse card basic, added global styling

---
+++ ahp-client/src/components/HorseCards/HorseCardLarge.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Card } from "react-bootstrap";
+
+const HorseCard = () => {
+  return (
+    <Card className="horse-card horse-card-larger">
+        {/* This will render the horse image */}
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <Card.Title>Horse Name </Card.Title>
+        <Card.Text>
+          <div>
+            <p>Horse Type</p>
+          </div>
+          <div>
+            <p>Horse Age</p>
+          </div>
+          <div>
+            <p>Distance</p>
+          </div>
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default HorseCard;
